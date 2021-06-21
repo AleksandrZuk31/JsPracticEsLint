b4d29dd9ebc991acb74362b3d6b8a28aeb8c1e92
Aleksand Zuk <Aleksandrzuk30013010@gmail.com>
2021-06-21 23:05:08 +0300

    выполнил 18 задание

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
-    const deadline = '21 june 2021';
+    const deadline = '22 june 2021';
     function countTimer(deadline) {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
@@ -53,4 +53,63 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setInterval(countTimer, 0, deadline);
+
+    //меню
+    const toggleMenu = () => {
+        const intViewportWidth = window.innerWidth;
+        console.log(intViewportWidth);
+        if (intViewportWidth > 768) {
+
+            const btnMenu = document.querySelector('.menu'),
+                menu = document.querySelector('menu'),
+                closeBtn = document.querySelector('.close-btn'),
+                menuItems = menu.querySelectorAll('ul>li');
+
+            const handlerMenu = () => {
+                const width = document.documentElement.clientWidth;
+                console.log(1);
+                if (menu.style.left === '0px') {
+                    console.log(1);
+                    const start = Date.now();
+                    const timer = setInterval(() => {
+                        const timePassed = Date.now() - start;
+                        menu.style.left = timePassed + 'px';
+                        if (timePassed > width) {
+                            clearInterval(timer);
+                        }
+                    }, 10);
+                } else {
+                    console.log(0);
+                    menu.style.left = '0px';
+                }
+            };
+
+            btnMenu.addEventListener('click', handlerMenu);
+            closeBtn.addEventListener('click', handlerMenu);
+            menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+        }
+
+    };
+
+    toggleMenu();
+
+    // popup
+
+    const togglePopup = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            closePopupBtn = document.querySelector('.popup-close');
+
+
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+            });
+        });
+        closePopupBtn.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+    };
+
+    togglePopup();
 });
