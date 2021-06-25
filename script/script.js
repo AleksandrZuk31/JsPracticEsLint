523e6f4c3643af69fd17dd4dd43ca1961deadaef
Aleksand Zuk <Aleksandrzuk30013010@gmail.com>
2021-06-25 19:31:50 +0300

    добавил делегирование на обработчики событий

--- script/script.js
+++ script/script.js
@@ -1,13 +1,12 @@
-
-
 window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
-    const deadline = '22 june 2021';
+    const deadline = '26 june 2021';
+
     function countTimer(deadline) {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
-            timerSeconds =  document.querySelector('#timer-seconds');
+            timerSeconds = document.querySelector('#timer-seconds');
 
         function getTimeRemaining() {
             const dateStop = new Date(deadline).getTime(),
@@ -16,7 +15,12 @@ window.addEventListener('DOMContentLoaded', () => {
                 seconds = Math.floor(timeRemaining % 60),
                 minutes = Math.floor((timeRemaining / 60) % 60),
                 hours = Math.floor(timeRemaining / 60 / 60);
-            return { timeRemaining, hours, minutes, seconds };
+            return {
+                timeRemaining,
+                hours,
+                minutes,
+                seconds
+            };
         }
 
         function updateClock() {
@@ -60,14 +64,12 @@ window.addEventListener('DOMContentLoaded', () => {
         let isOpenMenu = false;
         if (intViewportWidth > 768) {
 
-            const btnMenu = document.querySelector('.menu'),
-                menu = document.querySelector('menu'),
-                closeBtn = document.querySelector('.close-btn'),
-                menuItems = menu.querySelectorAll('ul>li');
-            
+            const menu = document.querySelector('menu'),
+                header = document.querySelector('header');
+
             const handlerMenu = () => {
                 const width = document.documentElement.clientWidth;
-                  if (!isOpenMenu) {
+                if (!isOpenMenu) {
                     const start = Date.now();
                     const timer = setInterval(() => {
                         const timePassed = Date.now() - start;
@@ -82,13 +84,31 @@ window.addEventListener('DOMContentLoaded', () => {
                     menu.style.left = '0px';
                 }
             };
+            menu.addEventListener('click', (event) => {
+                let target = event.target;
+                if (target.tagName === ('A')) {
+                    handlerMenu();
+                } else if (target.classList.contains('close-btn')) {
+                    handlerMenu();
+                }
+                return;
+            });
+            header.addEventListener('click', (event) => {
+                let target = event.target;
+                if (target.classList.contains('menu')) {
+                    handlerMenu();
+                } else {
+                    target = target.closest('.menu');
+                }
+                if (target !== null) {
+                    handlerMenu();
+                }
+                return;
+            });
 
-            btnMenu.addEventListener('click', handlerMenu);
-            closeBtn.addEventListener('click', handlerMenu);
-            menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
-         } else {
+        } else {
             return;
-         }
+        }
 
     };
 
@@ -98,19 +118,69 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const togglePopup = () => {
         const popup = document.querySelector('.popup'),
-            popupBtn = document.querySelectorAll('.popup-btn'),
-            closePopupBtn = document.querySelector('.popup-close');
-
+            popupBtn = document.querySelectorAll('.popup-btn');
 
         popupBtn.forEach(elem => {
             elem.addEventListener('click', () => {
                 popup.style.display = 'block';
             });
         });
-        closePopupBtn.addEventListener('click', () => {
-            popup.style.display = 'none';
-        });
+
+        popup.addEventListener('click', (event) => {
+            let target = event.target;
+
+            if (target.classList.contains('popup-close')) {
+                popup.style.display = 'none';
+            } else {
+                target = target.closest('.popup-content');
+            }
+            if (!target) {
+                popup.style.display = 'none';
+            }
+
+        })
+
     };
 
     togglePopup();
+
+    // табы
+
+    const tabs = () => {
+        const tabHeader = document.querySelector('.service-header'),
+            tab = tabHeader.querySelectorAll('.service-header-tab'),
+            tabContent = document.querySelectorAll('.service-tab');
+
+        const toggleTabContent = (index) => {
+            for (let i = 0; i < tabContent.length; i++) {
+                if (index === i) {
+                    tab[i].classList.add('active');
+                    tabContent[i].classList.remove('d-none');
+                } else {
+                    tab[i].classList.remove('active');
+                    tabContent[i].classList.add('d-none');
+                }
+            }
+
+        };
+
+        tabHeader.addEventListener('click', (event) => {
+            let target = event.target;
+
+            target = target.closest('.service-header-tab');
+            if (target) {
+
+                tab.forEach((item, i) => {
+                    if (item === target) {
+                        toggleTabContent(i);
+                    }
+                });
+            }
+
+
+        });
+    };
+
+    tabs();
+
 });
