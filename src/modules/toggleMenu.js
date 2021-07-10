fd972c81b81397f663b7e594304a07c4b7eaccca
Aleksand Zuk <Aleksandrzuk30013010@gmail.com>
2021-07-10 19:28:38 +0300

    исправлено

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -5,24 +5,36 @@ const toggleMenu = () => {
         header = document.querySelector('header');
 
     const handlerMenu = () => {
-        const width = document.documentElement.clientWidth;
-        if (!isOpenMenu) {
-            const start = Date.now();
-            const timer = setInterval(() => {
-                const timePassed = Date.now() - start;
-                menu.style.left = timePassed + 'px';
-                if (timePassed > width) {
-                    clearInterval(timer);
-                }
-            }, 10);
-            isOpenMenu = true;
+        if (window.innerWidth > 768) {
+            const width = document.documentElement.clientWidth;
+            if (!isOpenMenu) {
+                const start = Date.now();
+                const timer = setInterval(() => {
+                    const timePassed = Date.now() - start;
+                    menu.style.left = timePassed + 'px';
+                    if (timePassed > width) {
+                        clearInterval(timer);
+                    }
+                }, 10);
+                isOpenMenu = true;
+            } else {
+                isOpenMenu = false;
+                menu.style.left = '0px';
+            }
         } else {
-            isOpenMenu = false;
-            menu.style.left = '0px';
+            if (!isOpenMenu) {
+                const widthMenu = document.documentElement.clientWidth;
+                menu.style.left = widthMenu + 'px';
+                isOpenMenu = true;
+            } else {
+                isOpenMenu = false;
+                menu.style.left = '0px';
+            }
         }
-    };
-    menu.addEventListener('click', event => {
 
+    };
+
+    menu.addEventListener('click', event => {
         const target = event.target;
         if (target.tagName === ('A')) {
             handlerMenu();
@@ -34,21 +46,18 @@ const toggleMenu = () => {
     });
 
     header.addEventListener('click', event => {
-        if (window.innerWidth > 768) {
 
-            let target = event.target;
-            if (target.classList.contains('menu')) {
-                handlerMenu();
-            } else {
-                target = target.closest('.menu');
-            }
-            if (target !== null) {
-                handlerMenu();
-            }
-            return;
+        let target = event.target;
+        if (target.classList.contains('menu')) {
+            handlerMenu();
         } else {
-            return;
+            target = target.closest('.menu');
         }
+        if (target !== null) {
+            handlerMenu();
+        }
+        return;
+
     });
 
 };
