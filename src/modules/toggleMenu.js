56e464b638225adebac0e496a92b8c6753e639fe
Aleksand Zuk <Aleksandrzuk30013010@gmail.com>
2021-07-10 18:27:42 +0300

    меню работает

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,38 +1,41 @@
 const toggleMenu = () => {
 
-    const checkAnimate = () => {
+    let isOpenMenu = false;
+    const menu = document.querySelector('menu'),
+        header = document.querySelector('header');
 
-        let isOpenMenu = false;
-        const menu = document.querySelector('menu'),
-            header = document.querySelector('header');
+    const handlerMenu = () => {
+        const width = document.documentElement.clientWidth;
+        if (!isOpenMenu) {
+            const start = Date.now();
+            const timer = setInterval(() => {
+                const timePassed = Date.now() - start;
+                menu.style.left = timePassed + 'px';
+                if (timePassed > width) {
+                    clearInterval(timer);
+                }
+            }, 10);
+            isOpenMenu = true;
+        } else {
+            isOpenMenu = false;
+            menu.style.left = '0px';
+        }
+    };
+    menu.addEventListener('click', event => {
 
-        const handlerMenu = () => {
-            const width = document.documentElement.clientWidth;
-            if (!isOpenMenu) {
-                const start = Date.now();
-                const timer = setInterval(() => {
-                    const timePassed = Date.now() - start;
-                    menu.style.left = timePassed + 'px';
-                    if (timePassed > width) {
-                        clearInterval(timer);
-                    }
-                }, 10);
-                isOpenMenu = true;
-            } else {
-                isOpenMenu = false;
-                menu.style.left = '0px';
-            }
-        };
-        menu.addEventListener('click', event => {
-            const target = event.target;
-            if (target.tagName === ('A')) {
-                handlerMenu();
-            } else if (target.classList.contains('close-btn')) {
-                handlerMenu();
-            }
-            return;
-        });
-        header.addEventListener('click', event => {
+        const target = event.target;
+        if (target.tagName === ('A')) {
+            handlerMenu();
+        } else if (target.classList.contains('close-btn')) {
+            handlerMenu();
+        }
+        return;
+
+    });
+
+    header.addEventListener('click', event => {
+        if (window.innerWidth > 768) {
+
             let target = event.target;
             if (target.classList.contains('menu')) {
                 handlerMenu();
@@ -43,107 +46,10 @@ const toggleMenu = () => {
                 handlerMenu();
             }
             return;
-        });
-
-    };
-
-    if (window.innerWidth > 768) {
-        console.log(1);
-        checkAnimate();
-    } else {
-        return;
-    }
-
-    window.addEventListener('resize', () => {
-        console.log(2);
-        if (window.innerWidth > 768) {
-            console.log(3);
-            checkAnimate();
         } else {
-            console.log(4);
             return;
         }
-
     });
 
-    // let animate = false;
-
-    // const checkAnimate = () => {
-    //     console.log(animate);
-    //     if (animate) {
-    //         console.log(4, animate);
-
-    //         let isOpenMenu = false;
-    //         const menu = document.querySelector('menu'),
-    //             header = document.querySelector('header');
-
-    //         const handlerMenu = () => {
-    //             const width = document.documentElement.clientWidth;
-    //             if (!isOpenMenu) {
-    //                 const start = Date.now();
-    //                 const timer = setInterval(() => {
-    //                     const timePassed = Date.now() - start;
-    //                     menu.style.left = timePassed + 'px';
-    //                     if (timePassed > width) {
-    //                         clearInterval(timer);
-    //                     }
-    //                 }, 10);
-    //                 isOpenMenu = true;
-    //             } else {
-    //                 isOpenMenu = false;
-    //                 menu.style.left = '0px';
-    //             }
-    //         };
-    //         menu.addEventListener('click', event => {
-    //             const target = event.target;
-    //             if (target.tagName === ('A')) {
-    //                 handlerMenu();
-    //             } else if (target.classList.contains('close-btn')) {
-    //                 handlerMenu();
-    //             }
-    //             return;
-    //         });
-    //         header.addEventListener('click', event => {
-    //             let target = event.target;
-    //             if (target.classList.contains('menu')) {
-    //                 handlerMenu();
-    //             } else {
-    //                 target = target.closest('.menu');
-    //             }
-    //             if (target !== null) {
-    //                 handlerMenu();
-    //             }
-    //             return;
-    //         });
-    //     } else {
-    //         return;
-    //     }
-    // };
-
-    // if (window.innerWidth > 768) {
-    //     animate = true;
-    //     console.log(1);
-    //     checkAnimate();
-    // } else {
-    //     animate = false;
-    // }
-    // window.addEventListener('resize', () => {
-    //     console.log(2);
-    //     if (window.innerWidth > 768) {
-    //         animate = true;
-    //         console.log(3);
-    //         checkAnimate();
-    //         return;
-    //     } else {
-    //         animate = false;
-    //         console.log(5);
-
-    //         console.log(animate, 'animate');
-    //         checkAnimate();
-    //         return;
-
-    //     }
-    // });
 };
-
 export default toggleMenu;
